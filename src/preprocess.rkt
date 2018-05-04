a0f140c610a5995e2346118a4473999c55ddfef5
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-05-04 17:35:53 +0100

    Implement adding active tokens via a hash table

--- src/preprocess.rkt
+++ src/preprocess.rkt
@@ -1,13 +1,18 @@
 #lang racket
 (provide add-active-token def-active-token process-string)
 
-; TODO: Not yet implemented
-(define (add-active-token t) 
-  t)
+
+; Defines a hash map, mapping active tokens to functions
+; An active token's function is what must be executed when it is found
+(define active-tokens (make-hash))
 
 ; TODO: Not yet implemented
 (define (def-active-token t)
   t)
+; Adds a new mapping from token-name to token-function
+; WARNING: if token-name is already saved, IT WILL BE OVERWRITTEN!
+(define (add-active-token token-name token-function)
+  (hash-set! active-tokens token-name token-function))
 
 ; TODO: Not yet implemented
 (define (process-string s)
