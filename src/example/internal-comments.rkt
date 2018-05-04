07ce7ea062f7e6236305949c149373f4542132dd
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-05-04 17:38:21 +0100

    Add example for internal-comments in example dir

---
+++ src/example/internal-comments.rkt
@@ -0,0 +1,25 @@
+#lang racket
+(require "../preprocess.rkt")
+
+; +--------------------------+
+; | INTERNAL COMMENT EXAMPLE |
+; +--------------------------+
+
+; Example string to test with internal comments active token ";;""
+(define ex-internal-comment
+#<<END
+//Another great idea from our beloved client
+;;This is stupid but it’s what the client wants
+for(int i = 0; i < MAX_SIZE; i++) {
+END
+)
+
+; Defines the ";;" active token to be used for internal comments
+(def-active-token ";;" (str)
+  (match (regexp-match-positions "\n" str)
+    ((list (cons start end)) (substring str end))
+    (else "")))
+
+; Process the string and display it to the user
+(displayln (process-string ex-internal-comment))
+
