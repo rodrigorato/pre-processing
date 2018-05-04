60c922311e20094879b2f635113d2663b1eb9bd9
Rodrigo Rato <rodrigorato@tecnico.ulisboa.pt>
2018-05-04 17:37:34 +0100

    Implement def-active-token via a new syntax rule, using add token mechanism

--- src/preprocess.rkt
+++ src/preprocess.rkt
@@ -9,13 +9,19 @@
 ; An active token's function is what must be executed when it is found
 (define active-tokens (make-hash))
 
-; TODO: Not yet implemented
-(define (def-active-token t)
-  t)
+; Gets an active token's function by its name
+(define (get-active-token-function token-name)
+  (hash-ref active-tokens token-name))
+
 ; Adds a new mapping from token-name to token-function
 ; WARNING: if token-name is already saved, IT WILL BE OVERWRITTEN!
 (define (add-active-token token-name token-function)
   (hash-set! active-tokens token-name token-function))
+
+; Defines a new syntax rule for def-active-token, parsing it and calling add-active-token
+; Creates a new lambda from the given code, and adds that in add-active-token
+(define-syntax-rule (def-active-token token-name token-func-args token-func-code)
+  (add-active-token token-name (lambda token-func-args token-func-code)))
 
 ; TODO: Not yet implemented
 (define (process-string s)
